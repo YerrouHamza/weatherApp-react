4030577effa587d5cfc2121500004c416dc363de
yerrouhamza <hamzayerrou5@yahoo.com>
2024-12-31 14:46:10 +0100

    craeted the textinput and theme mode

---
+++ src/components/themeMode.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { IconBulbFilled, IconBulbOff} from '@tabler/icons-react';
+
+enum Theme {
+    Light = 'light',
+    Dark = 'dark'
+}
+
+const ModeIcon = ({theme, className}: {theme: Theme; className?: string}) => {
+    if (theme === 'light') return <IconBulbFilled className={className} />
+
+    return <IconBulbOff className={className} />
+}
+
+export default function ThemeMode() {
+    const [theme, setTheme] = useState('light')
+
+    return (
+        <button 
+            className='flex flex-col items-center'
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+            <ModeIcon 
+                theme={theme as Theme} 
+                className='size-7 text-gray-500'
+            />
+        </button>
+    )
+}
