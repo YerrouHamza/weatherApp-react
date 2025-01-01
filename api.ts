78fe74de277316c1a5cd21d4de6a544bed53efb5
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-01 18:18:26 +0100

    created the api call function

---
+++ api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://api.weatherapi.com/v1/current.json",
+});
+
+api.interceptors.request.use(
+    (config) => {
+        config.params = {
+            key: '8ad0d366eba14f8087e220940241111',
+        };
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
