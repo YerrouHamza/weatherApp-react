69e5905fdcc3603ebfcbee44e82eca17c9c38509
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-02 17:27:06 +0100

    created the context for the loader and created the loader component

---
+++ src/context-api/loaderOverlayContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+type LoaderContextType = {
+    isLoading: boolean,
+    setIsLoading: (value: boolean) => void
+}
+
+export const LoaderOverlayContext = createContext<LoaderContextType | null>(null)
+
+
+export const LoaderOverlayProvider = ({children}: {children: React.ReactNode}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    return (
+        <LoaderOverlayContext.Provider value={{isLoading, setIsLoading}}>
+            {children}
+        </LoaderOverlayContext.Provider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { WeatherDataProvider } from './context-api/weatherDataContext'
 import './styles/global.css'
+import { WeatherDataProvider } from './context-api/weatherDataContext'
+import { LoaderOverlayProvider } from './context-api/loaderOverlayContext'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <WeatherDataProvider>
-      <App />
-    </WeatherDataProvider>
+    <LoaderOverlayProvider>
+      <WeatherDataProvider>
+        <App />
+      </WeatherDataProvider>
+    </LoaderOverlayProvider>
   </StrictMode>,
 )
