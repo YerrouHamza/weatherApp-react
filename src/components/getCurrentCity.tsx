bc7a69655820681d2f786d7ce01433be1a0398fd
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-03 15:21:31 +0100

    created demo function for get the current city

--- src/components/getCurrentCity.tsx
+++ src/components/getCurrentCity.tsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
+import { WeatherData } from '../context-api/weatherDataContext';
 import Button from './ui/button'
 
+
 export default function GetCurrentCity() {
+  const context = useContext(WeatherData);
+  if (!context) throw new Error("GetCurrentCity must be used within a WeatherDataProvider");
+
+  const {weatherDetails ,setCity} = context;
+
+  console.log(weatherDetails);
+  
+  const handelGetCurrentCity = async () => {
+    setCity('tetouan')
+  }
+
   return (
-    <Button variant="primary" className="text-sm">
+    <Button variant="primary" className="text-sm" onClick={handelGetCurrentCity}>
         Use current location
     </Button>
   )
