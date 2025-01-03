91c7e323b30bb64f0dcf1cdd87ead648ef6fb363
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-03 22:28:44 +0100

    give the controle bar the card

--- src/views/controlsBar.tsx
+++ src/views/controlsBar.tsx
@@ -1,13 +1,14 @@
 import GetCurrentCity from "../components/getCurrentCity";
 import ThemeMode from "../controllers/themeMode";
 import SerachClity from "../controllers/serachClity";
+import Card from "../components/ui/card";
 
 export default function ControlsBar() {
   return (
-    <div className="flex justify-between items-center p-4">
+    <Card className="h-fit" bodyClass="flex justify-between items-center p-5">
         <ThemeMode />
         <SerachClity />
         <GetCurrentCity />
-    </div>
+    </Card>
   )
 }
