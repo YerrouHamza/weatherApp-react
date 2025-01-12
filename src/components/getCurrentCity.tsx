669714e10260f20b9953978d9bc222c069633284
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-12 17:47:52 +0100

    added the loader to the get current loaction while it await to track the broswer location

--- src/components/getCurrentCity.tsx
+++ src/components/getCurrentCity.tsx
@@ -1,15 +1,21 @@
 import { useContext } from 'react';
 import { WeatherData } from '../context-api/weatherDataContext';
 import Button from './ui/button'
+import { LoaderOverlayContext } from '../context-api/loaderOverlayContext';
 
 
 export default function GetCurrentCity() {
   const wethaerContext = useContext(WeatherData);
+  const loaderContext = useContext(LoaderOverlayContext)
   if (!wethaerContext) throw new Error("GetCurrentCity must be used within a WeatherDataProvider");
+  if (!loaderContext) throw new Error('Error while get the loader context')
   const {fetchCity} = wethaerContext;
+  const {setIsLoading} = loaderContext
   
   const handelGetCurrentCity = async () => {
     if(!navigator.geolocation) return console.error('Geolocation is not supported by your browser');
+
+    setIsLoading(true)
     
     navigator.geolocation.getCurrentPosition((position) => {
       try {
@@ -18,6 +24,7 @@ export default function GetCurrentCity() {
         fetchCity(currentLocation)
       } catch (error) {
         console.error('Error while getting the current location', error)
+        setIsLoading(false)
       }
     });
   }
