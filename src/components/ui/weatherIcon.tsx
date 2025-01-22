128f731055a22501bc65da9a59aee1a7aaac2a53
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-22 15:45:48 +0100

    improved the wetaher condation code and grouped the csv file

--- src/components/ui/weatherIcon.tsx
+++ src/components/ui/weatherIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import WeatherStatus from "../../lib/weatherIconsStatus"
 import { cn } from "../../lib/utils"
+import WeatherStatus from "../../lib/weatherIconsStatus"
 
 type conditionIcon = {
     code: number
@@ -8,6 +8,11 @@ type conditionIcon = {
     isDay?: number,
     size: 'sm' | 'md' | "lg"
 }
+
+const cloudyCode = [1006, 1009, 1030, 1135, 1003];
+const snowCode = [1066, 1114, 1210, 1213, 1216, 1219, 1222, 1225, 1255, 1258, 1279, 1282, 1069, 1072, 1117, 1147, 1150, 1168, 1171, 1198, 1201, 1237, 1204, 1207, 1249, 1252, 1261, 1264];
+const rainCode = [1063, 1183, 1186, 1189, 1192, 1195, 1153, 1180, 1240, 1243, 1246];
+const thunderyCode = [1276, 1087, 1273]
 
 export default React.memo(function WeatherIconComponent({code, isDay, size, className}: conditionIcon) {
     const IconSize = {
@@ -17,15 +22,16 @@ export default React.memo(function WeatherIconComponent({code, isDay, size, clas
     }
     const iconWeatherProps = {className: cn(IconSize[size], className), alt: "Warther Icon"}
 
-    if ([1003, 1006, 1009].includes(code)) {
-        return <img src={WeatherStatus.Cloudy} {...iconWeatherProps} />
-    } else if (
-        [1063, 1183, 1186, 1189, 1192, 1195].includes(code)
-    ) {
-        return <img src={WeatherStatus.Rainy} {...iconWeatherProps} />
-    } else if ([1117, 1204, 1210, 1216].includes(code)) {
-        return <img src={WeatherStatus.WindSunny} {...iconWeatherProps} />
+    if (cloudyCode.includes(code)) {
+        if(code === 1003) return <img src={isDay === 0 ? WeatherStatus.PrettyCloudyMoonIcon : WeatherStatus.PrettyCloudyMoonIcon} {...iconWeatherProps} />
+        return <img src={WeatherStatus.CloudyIcon} {...iconWeatherProps} />
+    } else if (snowCode.includes(code)) {
+        return <img src={WeatherStatus.SnowIcon} {...iconWeatherProps} />
+    } else if (rainCode.includes(code)) {
+        return <img src={WeatherStatus.RainyIcon} {...iconWeatherProps} />
+    } else if (thunderyCode.includes(code)) {
+        if(code === 1087) return <img src={WeatherStatus.ThunderIcon} {...iconWeatherProps} />
+        return <img src={WeatherStatus.RainyThunderIcon} {...iconWeatherProps} />
     }
-
-    return <img src={isDay === 0 ? WeatherStatus.Moon : WeatherStatus.Sunny} {...iconWeatherProps} />
+    return <img src={isDay === 0 ? WeatherStatus.MoonIcon : WeatherStatus.SunnyIcon} {...iconWeatherProps} />
 })
--- src/lib/weatherIconsStatus.tsx
+++ src/lib/weatherIconsStatus.tsx
@@ -1,23 +1,31 @@
 import CloudyIcon from '../assets/weatherStatus/couldy.png'
-import RainyIcon from '../assets/weatherStatus/rain.png';
-import SunnyIcon from '../assets/weatherStatus/suny.png';
 import HotIcon from '../assets/weatherStatus/hot.png';
 import MoonIcon from '../assets/weatherStatus/moon.png';
 import PrettyCloudyMoonIcon from '../assets/weatherStatus/pretty-cloudy-moon.png';
 import PrettyCloudyIcon from '../assets/weatherStatus/pretty-cloudy.png';
+import RainyThunderIcon from '../assets/weatherStatus/rain-thunder.png';
+import RainyIcon from '../assets/weatherStatus/rain.png';
+import SnowIcon from '../assets/weatherStatus/snow.png';
+import SunnyIcon from '../assets/weatherStatus/suny.png';
+import ThunderIcon from '../assets/weatherStatus/thunder.png';
 import WindCloudyIcon from '../assets/weatherStatus/wind-cloudy.png'
 import WindSunnyIcon from '../assets/weatherStatus/wind-suny.png'
+import WindyIcon from '../assets/weatherStatus/windy.png'
 
 const WeatherStatus = {
-    Cloudy: CloudyIcon,
-    Rainy: RainyIcon,
-    Sunny: SunnyIcon,
-    Hot: HotIcon,
-    Moon: MoonIcon,
-    PrettyCloudyMoon: PrettyCloudyMoonIcon,
-    PrettyCloudy: PrettyCloudyIcon,
-    WindCloudy: WindCloudyIcon,
-    WindSunny: WindSunnyIcon
+    CloudyIcon,
+    HotIcon,
+    MoonIcon,
+    PrettyCloudyMoonIcon,
+    PrettyCloudyIcon,
+    RainyThunderIcon,
+    RainyIcon,
+    SnowIcon,
+    SunnyIcon,
+    ThunderIcon,
+    WindCloudyIcon,
+    WindSunnyIcon,
+    WindyIcon,
 };
 
 export default WeatherStatus;
